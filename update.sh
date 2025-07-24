9a484aa604e30e3888c8b2d05af8ad6990e9dd7c
Gogs <gogs@fake.local>
2025-07-24 11:29:01 +0330

    test3

--- update.sh
+++ update.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+echo "::group::Initializing F-Droid repository"
+fdroid init --verbose || true # Ignore if already initialized
+echo "Updating F-Droid repository"
+fdroid update --create-metadata --verbose
+echo "Signing F-Droid repository"
+fdroid sign --verbose
+echo "::endgroup::"
 
 cd metascoop
 echo "::group::Building metascoop executable"
